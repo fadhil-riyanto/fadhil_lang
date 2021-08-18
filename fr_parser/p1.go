fe77ee183e7329e34ba894d0673eac4c5223589f
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-08-18 21:49:45 +0700

    add new package konsol

--- fr_parser/p1.go
+++ fr_parser/p1.go
@@ -9,26 +9,28 @@ import (
 
 func Parse(syntax string) {
 	split_baris := strings.Split(syntax, "\n")
-	for counter, _ := range split_baris {
+	for counter, syntaxxx := range split_baris {
+		indexke1dan2 := syntaxxx[:2]
+		if indexke1dan2 == "f_" {
+			//parse fungsi
 
-		//parse fungsi
-		get_kurung_awal := strings.Split(syntax, "->")
-		pkgname := get_kurung_awal[0]
-		fnname := get_kurung_awal[1]
-
-		get_valuefunc := strings.Split(syntax, "::")
-		if len(get_valuefunc) < 3 {
-			log.Fatal("maaf, syntax error dibaris " + strconv.Itoa(counter+1))
-		}
-		vfunc := get_valuefunc
-
-		if pkgname == "konsol" {
-			if fnname == "print" {
-				mod_konsol.Print(vfunc[1])
-			} else if fnname == "println" {
-				mod_konsol.Println(vfunc[1])
+			get_kurung_awal := strings.Split(syntaxxx, "->")
+			pkgname := get_kurung_awal[0]
+			fnname := get_kurung_awal[1]
+			get_valuefunc := strings.Split(syntaxxx, "::")
+			if len(get_valuefunc) < 3 {
+				log.Fatal("maaf, syntax error dibaris " + strconv.Itoa(counter+1))
 			}
+			vfunc := get_valuefunc
 
+			if pkgname == "f_konsol" {
+				if fnname == "print" {
+					mod_konsol.Print(vfunc[1])
+				} else if fnname == "println" {
+					mod_konsol.Println(vfunc[1])
+				}
+
+			}
 		}
 
 		//if get_kurung_awal[1] == "konsol" {
